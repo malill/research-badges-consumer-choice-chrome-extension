a16b6c3c78bf9f77f8f50163d6b530d705777933
malill <markus.lill@gmx.de>
2022-11-29 17:07:14 +0100

    feat: minor change

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,7 +1,5 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
+// This script gets injected into any opened page whose URL matches the pattern defined in the manifest
+// (see "content_script" key). Several foreground scripts can be declared
 // and injected into the same or different pages.
 
 var $ = require("jquery");
