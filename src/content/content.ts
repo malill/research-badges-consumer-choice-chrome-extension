528a9a6e7e26ce0313e65070b5af244c9536111a
malill <markus.lill@gmx.de>
2023-01-31 17:46:18 +0100

    feat: create amazon content script

--- src/content/content.ts
+++ src/content/content.ts
@@ -2,16 +2,6 @@
 // (see "content_script" key). Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-var $ = require("jquery");
-
-// Remove Amazon highlights
-const AMAZON_BAGE_CLASS = "a-badge"
-const AMAZON_BAGE_REGION_CLASS = "a-badge-region"
-
-$(`.${AMAZON_BAGE_CLASS}`).css("opacity", "0");
-// $(`.${AMAZON_BAGE_REGION_CLASS}`).css("background-color", "#26c6da");
-
-
 // Do other stuff
 console.log("[pAI] Hello, I am pAI!!!!");
 const url = window.location.href
--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,45 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
+        amazon: path.resolve(__dirname, "src/content/amazon.ts"),
         content: path.resolve(__dirname, "src/content/content.ts"),
         service_worker: path.resolve(__dirname, "src/background/service_worker.ts")
-},
+    },
     output: {
-    path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
     },
-resolve: {
-    extensions: [".ts", ".js"],
+    resolve: {
+        extensions: [".ts", ".js"],
     },
-module: {
-    rules: [
-        { test: /\.css$/i, use: ["style-loader", "css-loader"] },
-        {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-        },
-    ],
+    module: {
+        rules: [
+            { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
     },
-plugins: [
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src/popup/popup.html"),
-        filename: 'popup.html'
-    }),
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src/settings/settings.html"),
-        filename: 'settings.html'
-    }),
-    new CopyPlugin({
-        patterns: [
-            { from: "public" },
-            { from: path.resolve(__dirname, "src/style"), to: path.resolve(__dirname, "dist/style") }
-        ]
-    }),
-    new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-    })
-]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/popup/popup.html"),
+            filename: 'popup.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/settings/settings.html"),
+            filename: 'settings.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "public" },
+                { from: path.resolve(__dirname, "src/style"), to: path.resolve(__dirname, "dist/style") }
+            ]
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 };
