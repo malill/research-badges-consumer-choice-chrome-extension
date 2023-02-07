d8d0fe9edd4e3c01b2874d4f0252c06a24db2c40
malill <markus.lill@gmx.de>
2023-02-07 08:46:04 +0100

    feat: mv AMA code to amazon.js

--- src/content/amazon.js
+++ src/content/amazon.js
@@ -2,6 +2,8 @@ import { CORE_SHORT_NAME, AMA_CSS_BAGE_CLASS, REST_API_URL } from "../config/con
 
 var $ = require("jquery");
 
+const url = window.location.href
+
 var badgeClasses = $(`.${AMA_CSS_BAGE_CLASS}`)
 
 console.log(`[${CORE_SHORT_NAME}] Removed badges from ${badgeClasses.length} products`)
@@ -9,3 +11,24 @@ console.log(`[${CORE_SHORT_NAME}] Removed badges from ${badgeClasses.length} pro
 $.get(REST_API_URL + "info", (res) => {
     console.log(res);
 })
+
+// Search events
+const data = {
+    user_id: 1,
+    item_ids: [1, 2],
+    type: 1,
+    url: url,
+    timestamp: Date.now()
+}
+
+const ecm_API = REST_API_URL + "event/"
+
+$.ajax({
+    url: ecm_API,
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    dataType: "json"
+});
+
+// Product detail pages events
--- src/content/core.js
+++ src/content/core.js
@@ -2,27 +2,9 @@
 // (see "content_script" key). Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-import { CORE_SHORT_NAME, REST_API_URL } from "../config/constants"
-
-var $ = require("jquery");
+import { CORE_SHORT_NAME } from "../config/constants"
 
 // Do other stuff
 console.log(`[${CORE_SHORT_NAME}] Hello, I am a Bot!!!!`);
 const url = window.location.href
 console.log(`[${CORE_SHORT_NAME}]`, "You are visiting:", url)
-
-const data = {
-    user_id: 1,
-    type: 1,
-    url: url
-}
-
-const ecm_API = REST_API_URL + "event/"
-
-$.ajax({
-    url: ecm_API,
-    type: "POST",
-    data: JSON.stringify(data),
-    contentType: "application/json",
-    dataType: "json"
-});
