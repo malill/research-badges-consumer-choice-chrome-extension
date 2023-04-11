734a6d451feaf4669299a03d2f2e3fabaea32b51
Markus Lill <markus.lill@gmx.de>
2023-04-11 12:26:40 +0200

    fix: add to cart popup

--- src/content/amazon/v2/productDetailPage.js
+++ src/content/amazon/v2/productDetailPage.js
@@ -7,21 +7,25 @@ import { Event } from "./model/Event";
 let productNavigatorData = new ProductNavigatorData();
 
 // Emit the "inspect" event
-const asin = document.getElementById('addToCart_feature_div').getAttribute("data-csa-c-asin");
+let addToCartDivElement = document.getElementById("addToCart_feature_div");
+const asin = addToCartDivElement.getAttribute("data-csa-c-asin");
 let item = new AmazonPDPItem(asin);
 let event = new Event(item, "inspect");
 productNavigatorData.pushEvent(event);
 
 try {
     if (productNavigatorData.user.id != PRL_MISSING_VALUE) {
-        // Disable the add to cart button, make the add to cart button unresponsive
-        const addToCartEl = document.getElementById('add-to-cart-button');
-        addToCartEl.remove();
+        // Remove the add to cart input button (note: it is already disabled by blank.css, but will be activated again below)
+        const addToCartInputElement = document.getElementById('add-to-cart-button');
+        addToCartInputElement.remove();
 
-        document.getElementById("addToCart_feature_div").addEventListener("click", () => {
+        addToCartDivElement.addEventListener("click", () => {
             alert("Thank you for participating in our survey. You can close this tab now and return to the questionaire to answer a few more final questions.");
             // Simulate an HTTP redirect:
             // window.location.href = "http://www.w3schools.com";
         });
     }
 } catch (error) { }
+
+// Make the add-to-cart div element clickable again
+addToCartDivElement.style['pointer-events'] = "auto";
