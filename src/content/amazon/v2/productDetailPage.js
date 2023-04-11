c2db2e8850c1da1c988351d9a34e87a7e02f8f39
Markus Lill <markus.lill@gmx.de>
2023-04-11 12:14:02 +0200

    fix: blank css from pdp to search

--- src/content/amazon/v2/productDetailPage.js
+++ src/content/amazon/v2/productDetailPage.js
@@ -12,8 +12,6 @@ let item = new AmazonPDPItem(asin);
 let event = new Event(item, "inspect");
 productNavigatorData.pushEvent(event);
 
-
-
 try {
     if (productNavigatorData.user.id != PRL_MISSING_VALUE) {
         // Disable the add to cart button, make the add to cart button unresponsive
@@ -21,10 +19,9 @@ try {
         addToCartEl.remove();
 
         document.getElementById("addToCart_feature_div").addEventListener("click", () => {
-            alert("Thank you for participating in our survey. You are redirect to the questionaire to answer a few more questions.");
+            alert("Thank you for participating in our survey. You can close this tab now and return to the questionaire to answer a few more final questions.");
             // Simulate an HTTP redirect:
-            window.location.href = "http://www.w3schools.com";
+            // window.location.href = "http://www.w3schools.com";
         });
     }
 } catch (error) { }
-
