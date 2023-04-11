cdbfed4c7b2a688a5fc84434e0f49dc03a313ae4
Markus Lill <markus.lill@gmx.de>
2023-04-11 12:57:32 +0200

    emit add to cart event

--- src/content/amazon/v2/productDetailPage.js
+++ src/content/amazon/v2/productDetailPage.js
@@ -6,12 +6,14 @@ import { Event } from "./model/Event";
 // Create the datalayer object, responsible for tracking
 let productNavigatorData = new ProductNavigatorData();
 
-// Emit the "inspect" event
+// Get the parent div element of add-to-cart
 let addToCartDivElement = document.getElementById("addToCart_feature_div");
+
+// Emit the "inspect" event
 const asin = addToCartDivElement.getAttribute("data-csa-c-asin");
 let item = new AmazonPDPItem(asin);
-let event = new Event(item, "inspect");
-productNavigatorData.pushEvent(event);
+let inspectEvent = new Event(item, "inspect");
+productNavigatorData.pushEvent(inspectEvent);
 
 if (productNavigatorData.user.id != PRL_MISSING_VALUE) {
     // User came from Prolific (control or treatment), customize add-to-cart function
@@ -21,9 +23,11 @@ if (productNavigatorData.user.id != PRL_MISSING_VALUE) {
         addToCartInputElement.remove();
 
         addToCartDivElement.addEventListener("click", () => {
+            let addToCartEvent = new Event(item, "add-to-cart");
+            productNavigatorData.pushEvent(addToCartEvent);
             alert("Thank you for participating in our survey. Please hit the blue 'Ok' button and you will be redicted to the questionaire to answer a few more final questions.");
             // Simulate an HTTP redirect:
-            window.location.href = "http://www.w3schools.com";
+            // window.location.href = "http://www.w3schools.com";
         });
     } catch (error) { }
 }
