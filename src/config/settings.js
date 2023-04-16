dfd75675ec72b2093b561952d6686d85c2920e54
Markus Lill <markus.lill@gmx.de>
2023-04-16 19:41:10 +0200

    refactor

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,22 +1,16 @@
-// -------------------
-// Endpoint
-
+// Endpoint configuration
 // DEVELOPMENT
 export const REST_API_URL = "http://localhost:8000/event/";
-
 // STAGING
 // export const REST_API_URL = "https://product-navigator-staging.herokuapp.com/event/"
-
 // PRODUCTION
 // export const REST_API_URL = "https://product-navigator.herokuapp.com/event/"
 
 
-// -------------------
 // User configuration
 export const COOKIE_NAME_USER_ID = 'pnUID';
 
 
-// -------------------
 // Task configuration
 export const COOKIE_NAME_TASK_USER_ID = 'pnTaskUID';
 export const COOKIE_NAME_TASK_ID = 'pnTaskID';
@@ -26,7 +20,6 @@ export const COOKIE_VALUE_TASK_ID_TREATMENT = 'i7tM';
 export const COOKIE_VALUE_MISSING = 'none';
 
 
-// -------------------
 // Cookie durability
 export const COOKIE_LIFETIME_1HOUR = 1;
 export const COOKIE_LIFETIME_1DAY = 24;
@@ -35,7 +28,6 @@ export const COOKIE_LIFETIME_1MONTH = 24 * 7 * 4;
 export const COOKIE_LIFETIME_1YEAR = 24 * 7 * 52;
 
 
-// -------------------
 // Amazon Constants
 // Platform Constants | Amazon Basics
 export const AMA_URL = 'https://www.amazon';
