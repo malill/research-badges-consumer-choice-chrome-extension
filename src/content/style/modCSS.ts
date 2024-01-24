f36cf25ca03591529a500763acc3cfa223bb9c29
malill <markus.lill@gmx.de>
2024-01-24 15:32:13 +0100

    feat: mask bestseller

--- src/content/style/modCSS.ts
+++ src/content/style/modCSS.ts
@@ -7,7 +7,7 @@ export const modCSS_01 = `
 
 /* --- BADGES --- */
 /*Badge1, Highlight (Best Seller, Amazon's Choice)*/
-span.rush-component [data-component-props*='amazons-choice'] {
+span.rush-component [data-component-props*='best-seller'] {
     display: block !important;
 }
 `
